f0edf6ab80a30682323a555891edb2f2df78d3c3
Hrishikesh1122 <hrishi.ugavekar@gmail.com>
2021-06-07 19:39:34 +0530

    UC1 Create contact

---
+++ AddressBookHandling/src/services/AddressBook.java
@@ -0,0 +1,15 @@
+package services;
+/**
+ * Purpose : Address Book Handling
+ * @author Hrishikesh Ugavekar
+ * @version 1.0
+ * @since 07-06-2021
+ *
+ */
+public class AddressBook {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Address Book Handling");
+	}
+
+}
